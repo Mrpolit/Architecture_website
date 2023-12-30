a13f2eb2b86153208387461577824468fcddf9c0
Mrpolite <mojtabar145@gmail.com>
2023-12-31 01:10:50 +0330

    #

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function logoText(){
     let font = Screen*(12.8/1920)+"rem";
     text.style.fontSize = font;
     svg.style.width = getTextWidth(text.style.innerHTML,` ${font} verdana`)+"px";
-    svg.style.height = getTextWidth(text.style.innerHTML,` ${font} verdana`)/5+"px";
     text.innerHTML = text.textContent.replace(/\S/g, "<a class='space'>$&</a>");
     stroke();
 }
@@ -16,7 +15,7 @@ function stroke(){
     for (let index = 0; index < logo.length; index++) {
         logo[index].style.strokeDasharray = 700;
         logo[index].style.strokeDashoffset = 700;
-        logo[index].style.animation = `logoAnimation 3s ease forwards ${(index/10)*2+1}s`
+        logo[index].style.animation = ` logoAnimation 3s ease forwards ${(index/10)*2+1}s`;
     }
 }
 function getTextWidth(text, fontsize) {
@@ -25,4 +24,4 @@ function getTextWidth(text, fontsize) {
     context.font = fontsize;
     const metrics = context.measureText(text);
     return metrics.width;
-  }
+}
