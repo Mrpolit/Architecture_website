a7e9fde6d709599b32b913de7e2b68bc4719e078
Mrpolite <mojtabar145@gmail.com>
2024-01-01 02:23:01 +0330

    nav bar update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 logoText();
 setTimeout(introEnd,11000);
 
+var flag = false;
+
 function logoText(){
     let Screen = window.innerWidth;
     const text=document.querySelector('.text-logo');
-    const svg =document.querySelector("svg");
     let font = Screen*(12.8/1920)+"rem";
     text.style.fontSize = font;
     text.innerHTML = text.textContent.replace(/\S/g, "<a class='space'>$&</a>");
@@ -21,4 +22,17 @@ function stroke(){
 function introEnd() {
     const intro = document.querySelector('.intro');
     intro.style.display = "none";
+}
+function navbarbtn(){
+    const navbarButton = document.querySelector(".navBarButton");
+    const navbar = document.querySelector(".navBar");
+
+    if(flag){
+        navbar.style.display = "none";
+        navbarButton.style.animation = "rotatezero 1s ease forwards";
+    }else{
+        navbar.style.display = "flex";
+        navbarButton.style.animation = "rotateninty 1s ease forwards";
+    }
+    flag=!flag;
 }
