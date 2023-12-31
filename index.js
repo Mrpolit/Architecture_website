ff7b71da3789da5447485d8a98b764c87783b6fe
Mrpolite <mojtabar145@gmail.com>
2024-01-01 01:20:37 +0330

    intro image

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 logoText();
-setTimeout(introEnd,8000);
+setTimeout(introEnd,11000);
 
 function logoText(){
     let Screen = window.innerWidth;
@@ -7,7 +7,6 @@ function logoText(){
     const svg =document.querySelector("svg");
     let font = Screen*(12.8/1920)+"rem";
     text.style.fontSize = font;
-    
     text.innerHTML = text.textContent.replace(/\S/g, "<a class='space'>$&</a>");
     stroke();
 }
@@ -16,7 +15,7 @@ function stroke(){
     for (let index = 0; index < logo.length; index++) {
         logo[index].style.strokeDasharray = 700;
         logo[index].style.strokeDashoffset = 700;
-        logo[index].style.animation = ` logoAnimation 3s ease forwards ${(index/10)*2+1}s`;
+        logo[index].style.animation = ` logoAnimation 4s ease forwards ${(index/10)*2+1}s`;
     }
 }
 function introEnd() {
