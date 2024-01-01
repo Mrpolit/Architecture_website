cae2d2a6f748854cdb5e00c0d9d97d8c61ac1bef
Mrpolite <mojtabar145@gmail.com>
2024-01-01 15:13:44 +0330

    navbar

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const navbarButton = document.querySelector(".navBarButton");
 const navbar = document.querySelector(".navBar");
 const line = document.querySelectorAll(".line");
 const halfline = document.querySelector(".halfline");
-
+const navbarshadow = document.querySelector(".navbarshadow");
 
 logoText();
 setTimeout(introEnd,11000);
@@ -36,16 +36,26 @@ function navbarbtn(){
         navbar.style.animation = "fadeout 0.5s ease forwards ";
         setTimeout(navbarEnd, 1000);
         navbarButton.style.animation = "rotatezero 1s ease forwards";
+        navbarshadow.style.boxShadow="0 0 0.5rem black";
+        navbarshadow.style.backgroundColor = "white";
         line[0].style.width="1rem";
+        line[0].style.backgroundColor = "var(--baseColor)";
         line[1].style.width="1rem";
+        line[1].style.backgroundColor = "var(--baseColor)";
         halfline.style.width="1rem";
+        halfline.style.backgroundColor = "var(--baseColor)";
     }else{
         navbar.style.display = "flex";
         navbar.style.animation = "fadein 3s ease forwards ";
+        navbarshadow.style.boxShadow="0 0 0 black";
+        navbarshadow.style.backgroundColor = "transparent";
         navbarButton.style.animation = "rotateninty 1.5s ease forwards";
         line[0].style.width="3rem";
+        line[0].style.backgroundColor = "white";
         line[1].style.width="2.5rem";
+        line[1].style.backgroundColor = "white";
         halfline.style.width="1.5rem";
+        halfline.style.backgroundColor = "white";
     }
     flag=!flag;
 }
